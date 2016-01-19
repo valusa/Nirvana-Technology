399b3ab78b3b57bfea38ae43fbc04f4fe955560e
valusa <nvalusa@hotmail.com>
2016-01-19 00:05:47 -0500

    commmit

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ jQuery(function($) {
 			$(this).find('.timer').each(function () {
 				var $this = $(this);
 				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
-					duration: 2000,
+					duration: 5000,
 					easing: 'swing',
 					step: function () {
 						$this.text(Math.ceil(this.Counter));
